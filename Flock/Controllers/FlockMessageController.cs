d25f348f40389c8698695529b952cd4ad99d4b8c
puliCode <12345sunny@gmail.com>
2013-10-05 20:40:06 +0530

    Draft Autentication and LDAP query implementation
    work in progress

--- Flock/Controllers/FlockMessageController.cs
+++ Flock/Controllers/FlockMessageController.cs
@@ -1,50 +1,62 @@
-﻿//using System.Web.Services.Description;
-//using AttributeRouting;
-//using AttributeRouting.Web.Mvc;
-//using Flock.Facade;
-//using Flock.Facade.Interfaces;
-//using Flock.Models;
-//using Flock.Repositories.Interfaces;
-//using Newtonsoft.Json.Linq;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Net;
-//using System.Net.Http;
-//using System.Web.Http;
-//using System.Web.Http;
+﻿using System.DirectoryServices;
+using System.Web;
+using System.Web.Security;
+using System.Web.Services.Description;
+using AttributeRouting;
+using AttributeRouting.Web.Mvc;
+using Flock.Facade;
+using Flock.Facade.Interfaces;
+using Flock.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http;
 
-//namespace Flock.Controllers
-//{
-//    [RoutePrefix("/api/flockMessage")]
-//    public class FlockMessageController : ApiController
-//    {
-//        private readonly IMessageFacade _messageFacade;
+namespace Flock.Controllers
+{
+    [RoutePrefix("/api/flockMessage")]
+    public class FlockMessageController : ApiController
+    {
+        private readonly IMessageFacade _messageFacade;
 
-//        public FlockMessageController(IMessageFacade messageFacade)
-//        {
-//            _messageFacade = messageFacade;
-//        }
+        public FlockMessageController(IMessageFacade messageFacade)
+        {
+            _messageFacade = messageFacade;
+        }
 
-//        [GET("messages")]
-//        public IEnumerable<FlockMessage> GetAll()
-//        {
-//           return _messageFacade.GetAllMessages();
+        [GET("messages")]
+        public IEnumerable<FlockMessage> GetAll()
+        {
 
-//        }
+            var test = new DirectoryEntry();
 
-//        [POST("message")]
-//        public HttpResponseMessage Post(FlockMessage message)
-//        {
-//            _messageFacade.SaveMessage(message);
-//            return Request.CreateResponse(HttpStatusCode.Created, message);
-//        }
+            var s = new DirectorySearcher { Filter = "(&(objectCategory=person)(objectClass=user)(sAMAccountName=pulikals))", SearchRoot = test };
+            using(s)
+            {
+                var results = s.FindAll();
+            }
 
-//        [PUT("message")]
-//        public HttpResponseMessage Put(FlockMessage message)
-//        {
-//            _messageFacade.UpdateMessage(message);
-//            return Request.CreateResponse(HttpStatusCode.Created, message);
-//        }
-//    }
-//}
+            var indenty = HttpContext.Current.User.Identity.Name;
+            return _messageFacade.GetAllMessages();
+
+        }
+
+        [POST("message")]
+        public HttpResponseMessage Post(FlockMessage message)
+        {
+            _messageFacade.SaveMessage(message);
+            return Request.CreateResponse(HttpStatusCode.Created, message);
+        }
+
+        [PUT("message")]
+        public HttpResponseMessage Put(FlockMessage message)
+        {
+            _messageFacade.UpdateMessage(message);
+            return Request.CreateResponse(HttpStatusCode.Created, message);
+        }
+    }
+}
--- Flock/Facade/Concrete/MessageFacade.cs
+++ Flock/Facade/Concrete/MessageFacade.cs
@@ -1,64 +1,78 @@
-﻿//using System.Collections.Generic;
-//using System.Linq;
-//using Flock.Facade.Interfaces;
-//using Flock.Models;
-//using System;
-//using Flock.Repositories.Interfaces;
-
-//namespace Flock.Facade.Concrete
-//{
-//    public class MessageFacade : IMessageFacade
-//    {
-//        private readonly IMessageRepository _messageRepository;
-
-//        public MessageFacade(IMessageRepository messageRepository)
-//        {
-//            _messageRepository = messageRepository;
-//        }
-
-//        public void SaveMessage(FlockMessage message)
-//        {
-//            message.CreateDateTime = DateTime.Now;
-//            _messageRepository.Add(message);
-//        }
-
-//        public void UpdateMessage(FlockMessage message)
-//        {
-//            _messageRepository.Update(message);
-//        }
+﻿using System.Collections.Generic;
+using System.Linq;
+using Flock.Facade.Interfaces;
+using Flock.Models;
+using System;
 
 
-//        public IEnumerable<FlockMessage> GetAllMessages()
-//        {
-//            var messages=_messageRepository.GetAll();
-//            var flockMessages = messages as FlockMessage[] ?? messages.ToArray();
-//            foreach(var message in flockMessages )
-//            {
-//                TimeSpan timeSpan  = DateTime.Now.Subtract(message.CreateDateTime);
-//                if(timeSpan.Days>2)
-//                {
-//                    message.LastUpdated = message.CreateDateTime.ToString("MMMM dd, yyyy") + " at " +
-//                                          message.CreateDateTime.ToString("hh:mm tt");
-//                }
-//                else if (timeSpan.Days > 0)
-//                {
-//                    message.LastUpdated = timeSpan.Days + " days ago";
-//                }
-//                else if(timeSpan.Hours>0 )
-//                {
-//                    message.LastUpdated = timeSpan.Hours + " hours ago";
-//                }
-//                else if(timeSpan.Minutes>0 )
-//                {
-//                    message.LastUpdated = timeSpan.Minutes + " minutes ago";
-//                }
-//                else
-//                {
-//                    message.LastUpdated = "Few seconds ago";
-//                } 
+namespace Flock.Facade.Concrete
+{
+    public class MessageFacade : IMessageFacade
+    {
+        //private readonly IMessageRepository _messageRepository;
 
-//            }
-//            return flockMessages.OrderByDescending(x=>x.CreateDateTime);
-//        }
-//    }
-//}
+        //public MessageFacade(IMessageRepository messageRepository)
+        //{
+        //    _messageRepository = messageRepository;
+        //}
+
+        //public void SaveMessage(FlockMessage message)
+        //{
+        //    message.CreateDateTime = DateTime.Now;
+        //    _messageRepository.Add(message);
+        //}
+
+        //public void UpdateMessage(FlockMessage message)
+        //{
+        //    _messageRepository.Update(message);
+        //}
+
+
+        //public IEnumerable<FlockMessage> GetAllMessages()
+        //{
+        //    var messages = _messageRepository.GetAll();
+        //    var flockMessages = messages as FlockMessage[] ?? messages.ToArray();
+        //    foreach (var message in flockMessages)
+        //    {
+        //        TimeSpan timeSpan = DateTime.Now.Subtract(message.CreateDateTime);
+        //        if (timeSpan.Days > 2)
+        //        {
+        //            message.LastUpdated = message.CreateDateTime.ToString("MMMM dd, yyyy") + " at " +
+        //                                  message.CreateDateTime.ToString("hh:mm tt");
+        //        }
+        //        else if (timeSpan.Days > 0)
+        //        {
+        //            message.LastUpdated = timeSpan.Days + " days ago";
+        //        }
+        //        else if (timeSpan.Hours > 0)
+        //        {
+        //            message.LastUpdated = timeSpan.Hours + " hours ago";
+        //        }
+        //        else if (timeSpan.Minutes > 0)
+        //        {
+        //            message.LastUpdated = timeSpan.Minutes + " minutes ago";
+        //        }
+        //        else
+        //        {
+        //            message.LastUpdated = "Few seconds ago";
+        //        }
+
+        //    }
+        //    return flockMessages.OrderByDescending(x => x.CreateDateTime);
+        //}
+        public void SaveMessage(FlockMessage message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<FlockMessage> GetAllMessages()
+        {
+            return null;
+        }
+
+        public void UpdateMessage(FlockMessage message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
