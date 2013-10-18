96300973de5d8ea5a7d41e49df2c245bdb537c17
VenCKi <kvkirthy@gmail.com>
2013-10-18 18:46:37 +0530

    Integration for getting all quacks

--- Flock/JSApplication/Controllers/userPageController.js
+++ Flock/JSApplication/Controllers/userPageController.js
@@ -3,6 +3,15 @@
 flockApp.controller('userPageController', function ($scope , userService, quackService) {
     $scope.user = {};
 
+    $scope.quacks = [];
+
+    quackService.getAllQuacks().then(function (data) {
+        if (data && _.isArray(data)) {
+            $scope.quacks = data;
+            console.log($scope.quacks);
+        }
+    });
+
     userService.getUser().then(function (user) {
         $scope.user = user;
     });
