3578a1b562e738b185c1fad33d2d32d6abedfaf2
VenCKi <kvkirthy@gmail.com>
2013-10-22 12:42:26 +0530

    Created User View

---
+++ Flock/Controllers/UserViewController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Flock.Controllers
+{
+    public class UserViewController : Controller
+    {
+        //
+        // GET: /UserView/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
