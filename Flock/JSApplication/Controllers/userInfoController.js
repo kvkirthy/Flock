5dc3208e7230d45f73c8399428cc98eaaa210faa
puliCode <12345sunny@gmail.com>
2013-11-02 01:25:26 +0530

    Fixed - focus to top of page on image click

--- Flock/JSApplication/Controllers/userInfoController.js
+++ Flock/JSApplication/Controllers/userInfoController.js
@@ -5,12 +5,15 @@ flockApp.controller('userInfoController', function ($scope, userService, quackSe
 
     $scope.$on('userInfoController.showUserInformation', function () {
         $scope.userPreferences = angular.copy($scope.user);
-        $scope.userPreferences.Project = $scope.userProject;
-        $scope.userPreferences.Interests = $scope.userInterest;
+         $scope.userProject=$scope.userPreferences.Project;
+         $scope.userInterest = $scope.userPreferences.Interests;
     });
 
     $scope.userPreferences = {};
     $scope.savePreferences = function () {
+        $scope.userPreferences.Project = $scope.userProject;
+        $scope.userPreferences.Interests = $scope.userInterest;
+
         userService.updateUserPreferences($scope.userPreferences).then(function () {
             $scope.userPreferences.Project = $scope.userProject;
             $scope.userPreferences.Interests = $scope.userInterest;
